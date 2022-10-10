a21b2b0518ec0bc07ddf244b822e04dbba962f34
Elena <agapitovae87@gmail.com>
2022-10-10 12:04:31 +0300

    Lection 3 is over

--- Lection3/Program.cs
+++ Lection3/Program.cs
@@ -114,7 +114,7 @@ newText = Replace(newText, 'c', 'C');
 Console.WriteLine(newText);
 Console.WriteLine();
 */
-
+/*
 int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
 
 void PrintArray(int[] array)
@@ -149,4 +149,38 @@ void SelectionSort(int[] array)
 PrintArray(arr);
 SelectionSort(arr);
 PrintArray(arr);
+*/
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
 
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition]) maxPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
